45e65c46e3ab52e248c111c30b79df5e42d32e71
KunalSingh2001 <kunalsingh787711@gmail.com>
2025-08-28 11:52:21 +0530

    Created layout page dysign

---
+++ src/components/elements/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Header() {
+    return (
+        <div className='bg-dark text-white d-flex justify-content-center align-items-center py-3'>
+                
+        </div>
+    );
+}
+
+export default Header;
